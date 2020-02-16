36dfc76311b9583f99be879a190a4a5ba6ab84ad
Ubuntu <mtadminnuvepro@mtadminnuvepro.jps54wsc2cxuxo5idgxytfvevb.cx.internal.cloudapp.net>
2020-02-16 15:14:38 +0000

    db removed

--- codingchallenegejan8/bankdata/src/main/java/com/mindtree/bankdata/exception/ErrorConstant.java
+++
@@ -1,7 +0,0 @@
-package com.mindtree.bankdata.exception;
-
-public class ErrorConstant {
-
-	public final static String BankNameAvailableException = "Bank Already present.";
-	public final static String UserIdNotFoundException = "User Id Not Available.";
-}
